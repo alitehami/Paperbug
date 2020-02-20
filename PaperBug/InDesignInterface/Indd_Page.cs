cccca2722643602d71e1c7850f4c494bd4a5ab9c
Ali Tehami <ali.tehami@woodsbagot.com>
2020-02-21 10:51:29 +1100

    pages data test

--- PaperBug/InDesignInterface/Indd_Page.cs
+++ PaperBug/InDesignInterface/Indd_Page.cs
@@ -21,5 +21,23 @@ namespace PaperBug.InDesignInterface
         {
             InDesignPage.Place(path);
         }
+
+        public List<PageItem> _AllPageItemsOfType(Page p, out List<string> names, string t = "test")
+        {
+            names = new List<string>();
+            List<PageItem> pi = new List<PageItem>();
+            foreach (PageItem i in p.AllPageItems)
+            {
+                PageItem pItem = i as PageItem;
+                string pType = pItem.Name;
+                names.Add(pType);
+                pi.Add(i as PageItem);
+            }
+           
+            return  pi;
+        }
+
+
+
     }
 }
